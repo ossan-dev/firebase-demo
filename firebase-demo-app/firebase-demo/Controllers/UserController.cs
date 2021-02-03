c4f2c83c5dea486479e6be11e1629411b02d8a5f
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-02-03 09:06:54 +0100

    add controller to test user registration and so on

---
+++ firebase-demo-app/firebase-demo/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firebase_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+            FirebaseApp.Create(new AppOptions() 
+            {
+                Credential = GoogleCredential.GetApplicationDefault()
+            });
+        }
+    }
+}
